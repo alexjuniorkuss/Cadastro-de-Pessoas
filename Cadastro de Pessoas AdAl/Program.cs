911321767a35480dc41f41b0b0225c1669a45a65
Alex <alexaleaxalex@gmail.com>
2021-07-19 09:58:02 -0300

    start project

---
+++ Cadastro de Pessoas AdAl/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadastro_de_Pessoas_AdAl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
