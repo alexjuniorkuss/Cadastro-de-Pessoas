b4e13d683c072f68f9eb11a4b2bb85c696142565
Gabriel Maurici <gabriel.maurici@hotmail.com>
2021-07-21 12:53:15 -0300

    Add Crud/Create

---
+++ Cadastro de Pessoas AdAl/Crud.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadastro_de_Pessoas_AdAl
+{
+    public delegate void Operacoes();
+    class Crud
+    {
+        protected Operacoes createOperation;
+        protected Operacoes readOperation;
+        protected Operacoes updateOperation;
+        protected Operacoes deleteOperation;
+
+        public void Create()
+        {
+            createOperation();
+        }
+        public void Read()
+        {
+            readOperation();
+        }
+        public void Update()
+        {
+            updateOperation();
+        }
+        public void Delete()
+        {
+            deleteOperation();
+        }
+    }
+
+}
---
+++ Cadastro de Pessoas AdAl/OperacaoPf.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadastro_de_Pessoas_AdAl
+{
+    class OperacaoPf : Crud
+    {
+        List<PessoaFisica> listPf = new List<PessoaFisica>();
+
+        public OperacaoPf()
+        {
+            this.createOperation = Create;
+            this.readOperation = Listar;
+            this.updateOperation = Alterar;
+            this.deleteOperation = Deletar;
+        }
+
+        private void Create()
+        {
+            Endereco endereco = new Endereco();
+            int id = 1;
+
+            Console.WriteLine("----------------- Cadastro Pessoa Física -----------------");
+
+            Console.Write("Digite seu Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Digite a data de nascimento: ");
+            DateTime data = Convert.ToDateTime(Console.ReadLine());
+            Console.Write("Digite seu CPF: ");
+            string cpf = Console.ReadLine();
+            Console.Write("Digite seu RG: ");
+            string rg = Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("----------------- Cadastro Pessoa Física -----------------");
+            Console.WriteLine("----------------- Cadastro de Endereço -----------------");
+            Console.Write("Digite sua Cidade: ");
+            endereco.Cidade = Console.ReadLine();
+            Console.Write("Digite seu Rua: ");
+            endereco.Rua = Console.ReadLine();
+            Console.Write("Digite seu Bairro: ");
+            endereco.Bairro = Console.ReadLine();
+            Console.Write("Digite o Número: ");
+            endereco.Numero = Console.ReadLine();
+            Console.Write("Complemento: ");
+            endereco.Complemento = Console.ReadLine();
+
+            PessoaFisica pf = new PessoaFisica(id, nome, data, endereco, cpf, rg);
+            listPf.Add(pf);
+            id++;
+        }
+
+    }
+}
