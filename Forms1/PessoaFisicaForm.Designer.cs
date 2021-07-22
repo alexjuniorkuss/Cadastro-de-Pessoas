a2dd94b9c88b442965c3b8879672b50135db4dbc
Gabriel Maurici <gabriel.maurici@hotmail.com>
2021-07-22 17:27:42 -0300

    Add atualização de Cadastro Pessoa Física

--- Forms1/PessoaFisicaForm.Designer.cs
+++ Forms1/PessoaFisicaForm.Designer.cs
@@ -36,7 +36,6 @@ namespace Forms1
             this.lbCpf = new System.Windows.Forms.Label();
             this.lbRg = new System.Windows.Forms.Label();
             this.tbNome = new System.Windows.Forms.TextBox();
-            this.tbData = new System.Windows.Forms.TextBox();
             this.tbCpf = new System.Windows.Forms.TextBox();
             this.tbRg = new System.Windows.Forms.TextBox();
             this.lbCadPf = new System.Windows.Forms.Label();
@@ -51,9 +50,22 @@ namespace Forms1
             this.lbComplemento = new System.Windows.Forms.Label();
             this.tbComplemento = new System.Windows.Forms.TextBox();
             this.lbCadEnd = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dgCadPf = new System.Windows.Forms.DataGridView();
             this.lbPfCadastradas = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.dtData = new System.Windows.Forms.DateTimePicker();
+            this.btEditar = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.btDeletar = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.clid = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clNome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clData = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clCpf = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clRg = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clRua = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clNumero = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clComplemento = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clBairro = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clCidade = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgCadPf)).BeginInit();
             this.SuspendLayout();
             // 
             // btCadastrarPf
@@ -74,6 +86,7 @@ namespace Forms1
             this.btLimpar.TabIndex = 1;
             this.btLimpar.Text = "Limpar";
             this.btLimpar.UseVisualStyleBackColor = true;
+            this.btLimpar.Click += new System.EventHandler(this.btLimpar_Click);
             // 
             // lbNome
             // 
@@ -115,28 +128,21 @@ namespace Forms1
             // 
             this.tbNome.Location = new System.Drawing.Point(95, 74);
             this.tbNome.Name = "tbNome";
-            this.tbNome.Size = new System.Drawing.Size(164, 20);
+            this.tbNome.Size = new System.Drawing.Size(184, 20);
             this.tbNome.TabIndex = 3;
-            // 
-            // tbData
-            // 
-            this.tbData.Location = new System.Drawing.Point(95, 99);
-            this.tbData.Name = "tbData";
-            this.tbData.Size = new System.Drawing.Size(164, 20);
-            this.tbData.TabIndex = 3;
             // 
             // tbCpf
             // 
             this.tbCpf.Location = new System.Drawing.Point(95, 125);
             this.tbCpf.Name = "tbCpf";
-            this.tbCpf.Size = new System.Drawing.Size(164, 20);
+            this.tbCpf.Size = new System.Drawing.Size(184, 20);
             this.tbCpf.TabIndex = 3;
             // 
             // tbRg
             // 
             this.tbRg.Location = new System.Drawing.Point(95, 151);
             this.tbRg.Name = "tbRg";
-            this.tbRg.Size = new System.Drawing.Size(164, 20);
+            this.tbRg.Size = new System.Drawing.Size(184, 20);
             this.tbRg.TabIndex = 3;
             // 
             // lbCadPf
@@ -188,28 +194,28 @@ namespace Forms1
             // 
             this.tbRua.Location = new System.Drawing.Point(95, 234);
             this.tbRua.Name = "tbRua";
-            this.tbRua.Size = new System.Drawing.Size(164, 20);
+            this.tbRua.Size = new System.Drawing.Size(184, 20);
             this.tbRua.TabIndex = 3;
             // 
             // tbNumero
             // 
             this.tbNumero.Location = new System.Drawing.Point(95, 259);
             this.tbNumero.Name = "tbNumero";
-            this.tbNumero.Size = new System.Drawing.Size(164, 20);
+            this.tbNumero.Size = new System.Drawing.Size(184, 20);
             this.tbNumero.TabIndex = 3;
             // 
             // tbBairro
             // 
             this.tbBairro.Location = new System.Drawing.Point(95, 310);
             this.tbBairro.Name = "tbBairro";
-            this.tbBairro.Size = new System.Drawing.Size(164, 20);
+            this.tbBairro.Size = new System.Drawing.Size(184, 20);
             this.tbBairro.TabIndex = 3;
             // 
             // tbCidade
             // 
             this.tbCidade.Location = new System.Drawing.Point(95, 336);
             this.tbCidade.Name = "tbCidade";
-            this.tbCidade.Size = new System.Drawing.Size(164, 20);
+            this.tbCidade.Size = new System.Drawing.Size(184, 20);
             this.tbCidade.TabIndex = 3;
             // 
             // lbComplemento
@@ -225,7 +231,7 @@ namespace Forms1
             // 
             this.tbComplemento.Location = new System.Drawing.Point(95, 285);
             this.tbComplemento.Name = "tbComplemento";
-            this.tbComplemento.Size = new System.Drawing.Size(164, 20);
+            this.tbComplemento.Size = new System.Drawing.Size(184, 20);
             this.tbComplemento.TabIndex = 3;
             // 
             // lbCadEnd
@@ -237,13 +243,29 @@ namespace Forms1
             this.lbCadEnd.TabIndex = 2;
             this.lbCadEnd.Text = "Cadastro de Endereço";
             // 
-            // dataGridView1
+            // dgCadPf
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(346, 74);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(461, 295);
-            this.dataGridView1.TabIndex = 4;
+            this.dgCadPf.BackgroundColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.dgCadPf.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgCadPf.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.btEditar,
+            this.btDeletar,
+            this.clid,
+            this.clNome,
+            this.clData,
+            this.clCpf,
+            this.clRg,
+            this.clRua,
+            this.clNumero,
+            this.clComplemento,
+            this.clBairro,
+            this.clCidade});
+            this.dgCadPf.GridColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dgCadPf.Location = new System.Drawing.Point(285, 74);
+            this.dgCadPf.Name = "dgCadPf";
+            this.dgCadPf.Size = new System.Drawing.Size(635, 295);
+            this.dgCadPf.TabIndex = 4;
+            this.dgCadPf.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgCadPf_CellContentClick);
             // 
             // lbPfCadastradas
             // 
@@ -254,19 +276,94 @@ namespace Forms1
             this.lbPfCadastradas.TabIndex = 2;
             this.lbPfCadastradas.Text = "Pessoas Físicas Cadastradas";
             // 
+            // dtData
+            // 
+            this.dtData.CalendarMonthBackground = System.Drawing.SystemColors.Menu;
+            this.dtData.Location = new System.Drawing.Point(95, 100);
+            this.dtData.Name = "dtData";
+            this.dtData.Size = new System.Drawing.Size(184, 20);
+            this.dtData.TabIndex = 5;
+            // 
+            // btEditar
+            // 
+            this.btEditar.HeaderText = "Editar";
+            this.btEditar.MinimumWidth = 50;
+            this.btEditar.Name = "btEditar";
+            this.btEditar.Width = 50;
+            // 
+            // btDeletar
+            // 
+            this.btDeletar.HeaderText = "Deletar";
+            this.btDeletar.MinimumWidth = 50;
+            this.btDeletar.Name = "btDeletar";
+            this.btDeletar.Width = 50;
+            // 
+            // clid
+            // 
+            this.clid.HeaderText = "ID";
+            this.clid.Name = "clid";
+            this.clid.Width = 30;
+            // 
+            // clNome
+            // 
+            this.clNome.HeaderText = "Nome";
+            this.clNome.Name = "clNome";
+            // 
+            // clData
+            // 
+            this.clData.HeaderText = "Data";
+            this.clData.Name = "clData";
+            // 
+            // clCpf
+            // 
+            this.clCpf.HeaderText = "CPF";
+            this.clCpf.Name = "clCpf";
+            // 
+            // clRg
+            // 
+            this.clRg.HeaderText = "RG";
+            this.clRg.Name = "clRg";
+            // 
+            // clRua
+            // 
+            this.clRua.HeaderText = "Rua";
+            this.clRua.Name = "clRua";
+            // 
+            // clNumero
+            // 
+            this.clNumero.HeaderText = "Número";
+            this.clNumero.Name = "clNumero";
+            this.clNumero.Width = 50;
+            // 
+            // clComplemento
+            // 
+            this.clComplemento.HeaderText = "Complemento";
+            this.clComplemento.Name = "clComplemento";
+            // 
+            // clBairro
+            // 
+            this.clBairro.HeaderText = "Bairro";
+            this.clBairro.Name = "clBairro";
+            // 
+            // clCidade
+            // 
+            this.clCidade.HeaderText = "Cidade";
+            this.clCidade.Name = "clCidade";
+            // 
             // PessoaFisicaForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.ClientSize = new System.Drawing.Size(923, 450);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dtData);
+            this.Controls.Add(this.dgCadPf);
             this.Controls.Add(this.tbCidade);
             this.Controls.Add(this.tbRg);
             this.Controls.Add(this.tbComplemento);
             this.Controls.Add(this.tbBairro);
             this.Controls.Add(this.tbCpf);
             this.Controls.Add(this.tbNumero);
-            this.Controls.Add(this.tbData);
             this.Controls.Add(this.tbRua);
             this.Controls.Add(this.lbCidade);
             this.Controls.Add(this.lbComplemento);
@@ -285,7 +382,7 @@ namespace Forms1
             this.Controls.Add(this.btCadastrarPf);
             this.Name = "PessoaFisicaForm";
             this.Text = "PessoaFisicaForm";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgCadPf)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -300,7 +397,6 @@ namespace Forms1
         private System.Windows.Forms.Label lbCpf;
         private System.Windows.Forms.Label lbRg;
         private System.Windows.Forms.TextBox tbNome;
-        private System.Windows.Forms.TextBox tbData;
         private System.Windows.Forms.TextBox tbCpf;
         private System.Windows.Forms.TextBox tbRg;
         private System.Windows.Forms.Label lbCadPf;
@@ -315,7 +411,20 @@ namespace Forms1
         private System.Windows.Forms.Label lbComplemento;
         private System.Windows.Forms.TextBox tbComplemento;
         private System.Windows.Forms.Label lbCadEnd;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgCadPf;
         private System.Windows.Forms.Label lbPfCadastradas;
+        private System.Windows.Forms.DateTimePicker dtData;
+        private System.Windows.Forms.DataGridViewButtonColumn btEditar;
+        private System.Windows.Forms.DataGridViewButtonColumn btDeletar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clNome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clData;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clCpf;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clRg;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clRua;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clNumero;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clComplemento;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clBairro;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clCidade;
     }
 }
--- Forms1/PessoaFisicaForm.cs
+++ Forms1/PessoaFisicaForm.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 using CadastroPessoas;
 
 namespace Forms1
-{   
+{
 
     public partial class PessoaFisicaForm : Form
     {
@@ -19,16 +19,26 @@ namespace Forms1
         {
             InitializeComponent();
             ListaPf = new List<PessoaFisica>();
+            dgCadPf.AutoGenerateColumns = false;
+            dgCadPf.AllowUserToAddRows = false;
+
+            ListarCadastro();
+        }
+        private void btLimpar_Click(object sender, EventArgs e)
+        {
+            LimparCadastro();
         }
 
+        int id = 1;
+        int idEdicao = 0;
         private void btCadastrarPf_Click(object sender, EventArgs e)
         {
             PessoaFisica pessoaFisica = new PessoaFisica();
             Endereco endereco = new Endereco();
 
-            pessoaFisica.Id = this.ListaPf.Count + 1;
+            pessoaFisica.Id = idEdicao;
             pessoaFisica.Nome = tbNome.Text;
-            pessoaFisica.Data = Convert.ToDateTime(tbData.Text);
+            pessoaFisica.Data = Convert.ToDateTime(dtData.Text);
             pessoaFisica.CPF = tbCpf.Text;
             pessoaFisica.RG = tbRg.Text;
 
@@ -40,9 +50,74 @@ namespace Forms1
 
             pessoaFisica.End = endereco;
 
-            this.ListaPf.Add(pessoaFisica);
+            int indexId = ListaPf.FindIndex(x => x.Id == pessoaFisica.Id);
+            if(indexId == -1)
+            {
+                pessoaFisica.Id = id;
+                this.ListaPf.Add(pessoaFisica);
+                MessageBox.Show($"{pessoaFisica.Nome} Cadastrado(a) com Sucesso!!!");
+            }
+            else
+            {
+                this.ListaPf[indexId] = pessoaFisica;
+                MessageBox.Show($"Cadastro {pessoaFisica.Nome} Editado(a) com Sucesso!!!");
+            }
+            id++;
 
-            MessageBox.Show($"{pessoaFisica.Nome} Cadastrado(a) com Sucesso!!!");
+            ListarCadastro();
+
+            LimparCadastro();
+        }
+        private void dgCadPf_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int indice = e.ColumnIndex;
+
+            if(indice == 0)
+            {
+                PessoaFisica pessoaF = this.ListaPf[e.RowIndex];
+                idEdicao =  pessoaF.Id;
+                CarregarCadastro(pessoaF);
+            }
+        }
+
+        
+        private void LimparCadastro()
+        {
+            tbNome.Text = String.Empty;
+            dtData.Text = String.Empty;
+            tbCpf.Text = String.Empty;
+            tbRg.Text = String.Empty;
+
+            tbRua.Text = String.Empty;
+            tbNumero.Text = String.Empty;
+            tbComplemento.Text = String.Empty;
+            tbBairro.Text = String.Empty;
+            tbCidade.Text = String.Empty;
+        }
+        private void ListarCadastro()
+        {
+            dgCadPf.Rows.Clear();
+
+            foreach (PessoaFisica pessoaF in ListaPf)
+            {
+                dgCadPf.Rows.Add("Editar", "Deletar", pessoaF.Id, pessoaF.Nome, pessoaF.Data.ToShortDateString(), pessoaF.CPF, pessoaF.RG, pessoaF.End.Rua, pessoaF.End.Numero, pessoaF.End.Complemento, pessoaF.End.Bairro, pessoaF.End.Cidade);
+            }
+
+            dgCadPf.Refresh();
+        }
+        private void CarregarCadastro(PessoaFisica pessoaF)
+        {
+            this.tbNome.Text = pessoaF.Nome;
+            this.dtData.Text = pessoaF.Data.ToShortDateString();
+            this.tbCpf.Text = pessoaF.CPF;
+            this.tbRg.Text = pessoaF.RG;
+            
+
+            this.tbRua.Text = pessoaF.End.Rua;
+            this.tbNumero.Text = pessoaF.End.Numero.ToString();
+            this.tbComplemento.Text = pessoaF.End.Complemento;
+            this.tbBairro.Text = pessoaF.End.Bairro;
+            this.tbCidade.Text = pessoaF.End.Cidade;
         }
     }
 }
